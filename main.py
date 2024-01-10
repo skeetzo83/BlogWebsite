6c1f007753b31481196e8b7e38a2b0c56fcbd326
skeetzo83 <mpernato08@gmail.com>
2024-01-11 00:24:08 +0800

    Initial commit

--- main.py
+++ main.py
@@ -15,20 +15,6 @@ from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 # import smtplib
 
 
-'''
-Make sure the required packages are installed: 
-Open the Terminal in PyCharm (bottom left). 
-
-On Windows type:
-python -m pip install -r requirements.txt
-
-On MacOS type:
-pip3 install -r requirements.txt
-
-This will install the packages from the requirements.txt for this project.
-'''
-
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
 ckeditor = CKEditor(app)
